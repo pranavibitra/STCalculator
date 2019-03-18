cff65e84cb36fd07f64954977b5c97c03d646399
Pranu <Pranu@193.11.186.234>
2019-03-18 12:21:08 +0100

    first commit

---
+++ Cal1/src/main/java/STproj1/Cal1/Cal1.java
@@ -0,0 +1,10 @@
+package STproj1.Cal1;
+
+public class Cal1 {
+	public Cal1() {
+		
+	}
+	public int add(int a, int b){
+		return a+b;
+	}
+}
---
+++ Cal1/src/test/java/STproj1/Cal1/Caltest1.java
@@ -0,0 +1,22 @@
+package STproj1.Cal1;
+
+import static org.junit.Assert.*;
+import org.junit.BeforeClass;
+
+import org.junit.Test;
+
+public class Caltest1 {
+	private static Cal1 calc;
+	
+	@BeforeClass
+	public static void setup() {
+		calc = new Cal1();
+	}
+	
+	
+	@Test
+	public void testadd() {
+		assertEquals(5,calc.add(3, 2));
+	}
+
+}
